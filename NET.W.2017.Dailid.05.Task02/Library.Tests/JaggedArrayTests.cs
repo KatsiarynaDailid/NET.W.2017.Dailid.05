dcafab092971999b206f8f5ed4af63894369e993
Katsiaryna Dailid <Katsiaryna_Dailid@epam.com>
2017-10-29 13:16:09 +0300

    day 5, task 2

---
+++ NET.W.2017.Dailid.05.Task02/Library.Tests/JaggedArrayTests.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Library.Tests
+{
+    [TestClass]
+    public class JaggedArrayTests
+    {
+
+        [TestMethod]
+        public void BubbleSortBySumIncrease_JArray_SortedJArray()
+        {
+            #region Arrange
+
+            int[][] jArray = new int[3][];
+            jArray[0] = new int[3] { 1, 3, 5 };
+            jArray[1] = new int[2] { 2, 4 };
+            jArray[2] = new int[4] { 2, 4, 6, 8 };
+
+            int[][] arrangeArray = new int[3][];
+            arrangeArray[0] = new int[4] { 2, 4, 6, 8 };
+            arrangeArray[1] = new int[3] { 1, 3, 5 };
+            arrangeArray[2] = new int[2] { 2, 4 };
+
+            #endregion
+
+            JaggedArray.BubbleSortBySumIncrease(jArray);
+
+            CollectionAssert.AreEqual(arrangeArray[0], jArray[0]);
+            CollectionAssert.AreEqual(arrangeArray[1], jArray[1]);
+            CollectionAssert.AreEqual(arrangeArray[2], jArray[2]);
+
+        }
+
+        [TestMethod]
+        public void BubbleSortBySumDecrease_JArray_SortedJArray()
+        {
+            #region Arrange
+
+            int[][] jArray = new int[3][];
+            jArray[0] = new int[3] { 1, 3, 5 };
+            jArray[1] = new int[2] { 2, 4 };
+            jArray[2] = new int[4] { 2, 4, 6, 8 };
+
+            int[][] arrangeArray = new int[3][];
+            arrangeArray[0] = new int[2] { 2, 4 };
+            arrangeArray[1] = new int[3] { 1, 3, 5 };
+            arrangeArray[2] = new int[4] { 2, 4, 6, 8 };
+
+            #endregion
+
+            JaggedArray.BubbleSortBySumDecrease(jArray);
+
+            CollectionAssert.AreEqual(arrangeArray[0], jArray[0]);
+            CollectionAssert.AreEqual(arrangeArray[1], jArray[1]);
+            CollectionAssert.AreEqual(arrangeArray[2], jArray[2]);
+        }
+
+        [TestMethod]
+        public void BubbleSortByMaxElementIncrease_JArray_SortedJArray()
+        {
+            #region Arrange
+
+            int[][] jArray = new int[3][];
+            jArray[0] = new int[3] { 1, 2, 5 };
+            jArray[1] = new int[2] { 2, 1 };
+            jArray[2] = new int[4] { 2, 4, -10, 8 };
+
+            int[][] arrangeArray = new int[3][];
+            arrangeArray[0] = new int[2] { 2, 1 };
+            arrangeArray[1] = new int[3] { 1, 2, 5 };
+            arrangeArray[2] = new int[4] { 2, 4, -10, 8 };
+
+            #endregion
+
+            JaggedArray.BubbleSortByMaxElementIncrease(jArray);
+
+            CollectionAssert.AreEqual(arrangeArray[0], jArray[0]);
+            CollectionAssert.AreEqual(arrangeArray[1], jArray[1]);
+            CollectionAssert.AreEqual(arrangeArray[2], jArray[2]);
+        }
+
+        [TestMethod]
+        public void BubbleSortByMaxElementDecrease_JArray_SortedJArray()
+        {
+            #region Arrange
+
+            int[][] jArray = new int[3][];
+            jArray[0] = new int[3] { 1, 2, 5 };
+            jArray[1] = new int[2] { 2, 1 };
+            jArray[2] = new int[4] { 2, 4, -10, 8 };
+
+            int[][] arrangeArray = new int[3][];
+            arrangeArray[0] = new int[4] { 2, 4, -10, 8 };
+            arrangeArray[1] = new int[3] { 1, 2, 5 };
+            arrangeArray[2] = new int[2] { 2, 1 };
+
+            #endregion
+
+            JaggedArray.BubbleSortByMaxElementDecrease(jArray);
+
+            CollectionAssert.AreEqual(arrangeArray[0], jArray[0]);
+            CollectionAssert.AreEqual(arrangeArray[1], jArray[1]);
+            CollectionAssert.AreEqual(arrangeArray[2], jArray[2]);
+        }
+
+        [TestMethod]
+        public void BubbleSortByMinElementIncrease_JArray_SortedJArray()
+        {
+            #region Arrange
+
+            int[][] jArray = new int[3][];
+            jArray[0] = new int[4] { 2, 4, -10, 8 };
+            jArray[1] = new int[2] { -2, 1 };
+            jArray[2] = new int[3] { 1, 2, 5 };
+
+            int[][] arrangeArray = new int[3][];
+            arrangeArray[0] = new int[3] { 1, 2, 5 };
+            arrangeArray[1] = new int[2] { -2, 1 };
+            arrangeArray[2] = new int[4] { 2, 4, -10, 8 };
+
+            #endregion
+
+            JaggedArray.BubbleSortByMinElementIncrease(jArray);
+
+            CollectionAssert.AreEqual(arrangeArray[0], jArray[0]);
+            CollectionAssert.AreEqual(arrangeArray[1], jArray[1]);
+            CollectionAssert.AreEqual(arrangeArray[2], jArray[2]);
+        }
+
+        [TestMethod]
+        public void BubbleSortByMinElementDecrease_JArray_SortedJArray()
+        {
+            #region Arrange
+
+            int[][] jArray = new int[3][];
+            jArray[0] = new int[3] { 1, 2, 5 };
+            jArray[1] = new int[4] { 2, 4, -10, 8 };
+            jArray[2] = new int[2] { -2, 1 };
+
+            int[][] arrangeArray = new int[3][];
+            arrangeArray[0] = new int[4] { 2, 4, -10, 8 };
+            arrangeArray[1] = new int[2] { -2, 1 };
+            arrangeArray[2] = new int[3] { 1, 2, 5 };
+
+            #endregion
+
+            JaggedArray.BubbleSortByMinElementDecrease(jArray);
+
+            CollectionAssert.AreEqual(arrangeArray[0], jArray[0]);
+            CollectionAssert.AreEqual(arrangeArray[1], jArray[1]);
+            CollectionAssert.AreEqual(arrangeArray[2], jArray[2]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BubbleSortByMinElementDecrease_NullArray_Exception()
+        {
+            int[][] jArray = null;
+            JaggedArray.BubbleSortByMinElementDecrease(jArray);
+        }
+    }
+}
---
+++ NET.W.2017.Dailid.05.Task02/Library/JaggedArray.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public static class JaggedArray
+    {
+        /// <summary>
+        /// This method sorts jagged array by increasing of the matrix row sum, using bubble sort
+        /// </summary>
+        /// <param name="array">Jagged array</param>
+        /// <returns>Sorted jagged array</returns>
+        public static void BubbleSortBySumIncrease(int[][] array)
+        {
+            Verify(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (CountTheSumOfArray(array[j]) > CountTheSumOfArray(array[i]))
+                    {
+                        Swap(ref array[i], ref array[j]);                     
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method sorts jagged array by decreasing of the matrix row sum, using bubble sort
+        /// </summary>
+        /// <param name="array">Jagged array</param>
+        /// <returns>Sorted jagged array</returns>
+        public static void BubbleSortBySumDecrease(int[][] array)
+        {
+            Verify(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (CountTheSumOfArray(array[j]) < CountTheSumOfArray(array[i]))
+                    {
+                        Swap(ref array[i], ref array[j]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method sorts jagged array by increasing of the matrix row max elements, using bubble sort
+        /// </summary>
+        /// <param name="array">Jagged array</param>
+        /// <returns>Sorted jagged array</returns>
+        public static void BubbleSortByMaxElementIncrease(int[][] array)
+        {
+            Verify(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (FindMaxElement(array[j]) < FindMaxElement(array[i]))
+                    {
+                        Swap(ref array[i], ref array[j]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method sorts jagged array by decreasing of the matrix row max elements, using bubble sort
+        /// </summary>
+        /// <param name="array">Jagged array</param>
+        /// <returns>Sorted jagged array</returns>
+        public static void BubbleSortByMaxElementDecrease(int[][] array)
+        {
+            Verify(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (FindMaxElement(array[j]) > FindMaxElement(array[i]))
+                    {
+                        Swap(ref array[i], ref array[j]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method sorts jagged array by increasing of the matrix row min elements, using bubble sort
+        /// </summary>
+        /// <param name="array">Jagged array</param>
+        /// <returns>Sorted jagged array</returns>
+        public static void BubbleSortByMinElementIncrease(int[][] array)
+        {
+            Verify(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (FindMinElement(array[j]) > FindMinElement(array[i]))
+                    {
+                        Swap(ref array[i], ref array[j]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method sorts jagged array by decreasing of the matrix row min elements, using bubble sort
+        /// </summary>
+        /// <param name="array">Jagged array</param>
+        /// <returns>Sorted jagged array</returns>
+        public static void BubbleSortByMinElementDecrease(int[][] array)
+        {
+            Verify(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (FindMinElement(array[j]) < FindMinElement(array[i]))
+                    {
+                        Swap(ref array[i], ref array[j]);
+                    }
+                }
+            }
+        }
+
+        #region Private
+
+        private static void Verify(int[][] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException("Parameter cannot be null");
+        }
+
+        private static void Swap(ref int[] i, ref int[] j)
+        {
+            var temp = i;
+            i = j;
+            j = temp;
+        }
+
+        private static int CountTheSumOfArray(int[] array)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                checked
+                {
+                    sum += array[i];
+                }
+            }
+
+            return sum;
+        }
+
+        private static int FindMaxElement(int[] array)
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+            }
+
+            return max;
+        }
+
+        private static int FindMinElement(int[] array)
+        {
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+            }
+
+            return min;
+        }
+
+        #endregion
+    }
+}
